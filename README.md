6b8b8bc9664afae7865de9172617c4b8b90edcd1
Zhang Liangliang <zhangll@homewell.com.cn>
2020-04-23 11:00:52 +0800

    add README.md

---
+++ README.md
@@ -0,0 +1,85 @@
+## 背景
+目前在圈子sparkR圈子内，并没有可用的以java后端作为提交sparkR代码提交的方案，以快速迭代开发应用，支持以springboot后端进行开发
+## 使用指南
+可以详见test部分种的 TestRHttpClient.java部分
+方便进行提交代码的测试
+
+## how to use
+1. 配置maven
+```xml
+<dependency>
+    <groupId>com.hw</groupId>
+    <artifactId>transmitlayer</artifactId>
+    <version>0.0.1-SNAPSHOT</version>
+</dependency>
+```
+2. 自定以配置文件，以properties方式注入客户端
+ 配置详情请看   
+ src\main\java\com\hw\transmitlayer\client\RHttpConf.java
+```java
+String livyPort = "http://192.168.1.10:8080";
+uri = new URI(livyPort);
+// 创建properties 自动配置相关文件
+properties = new Properties();
+// 创建一个sparkr属性
+properties.setProperty(RHttpConf.Entry.CONNECTION_SESSION_KIND.key(), "sparkr");
+properties.setProperty(RHttpConf.Entry.CONNECTION_SESSION_MAX_SIZE.key(), "1");
+// 生成客户端
+RHttpClient rHttpClient = new RHttpClient(uri, new RHttpConf(properties));
+```
+3. use your r code segment
+```java
+ String rcode = code....
+```
+4. addListener and write your own business logic
+```java
+JobHandle submitcode = rHttpClient.submitcode(code.toString());
+        submitcode.addListener(new JobHandle.Listener<StatementResultWithCode>() {
+            @Override
+            public void onJobQueued(JobHandle<StatementResultWithCode> jobHandle) {
+
+            }
+
+            @Override
+            public void onJobStarted(JobHandle<StatementResultWithCode> jobHandle) {
+
+            }
+
+            @Override
+            public void onJobCancelled(JobHandle<StatementResultWithCode> jobHandle) {
+
+            }
+
+            @Override
+            public void onJobFailed(JobHandle<StatementResultWithCode> jobHandle, Throwable throwable) {
+                throwable.printStackTrace();
+            }
+
+            @Override
+            public void onJobSucceeded(JobHandle<StatementResultWithCode> jobHandle,
+                    StatementResultWithCode statementResultWithCode) {
+                System.out.println(statementResultWithCode.id);
+                System.out.println(statementResultWithCode.progress);
+                System.out.println(statementResultWithCode.state);
+                // try {
+                // Output output = new Output(new FileOutputStream("data/person.bin"));
+                // Kryo kryo = new Kryo();
+                // kryo.register(StatementResultWithCode.class);
+                // kryo.writeObject(output,statementResultWithCode);
+                // } catch (FileNotFoundException e) {
+                // e.printStackTrace();
+                // }
+                if (statementResultWithCode.output.data == null) {
+                    throw new RuntimeException("返回数据错误");
+                }
+                String result = statementResultWithCode.output.data.get("text/plain");
+                try {
+                    ResultClass[] resultClasses = new ObjectMapper().readValue(result, ResultClass[].class);
+                    System.out.println(resultClasses);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(statementResultWithCode.state);
+            }
+        });
+```
--- src/test/java/com/hw/transmitlayer/client/TestRHttpClient.java
+++ src/test/java/com/hw/transmitlayer/client/TestRHttpClient.java
@@ -17,51 +17,46 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = {TransmitlayerApplication.class})
+@SpringBootTest(classes = { TransmitlayerApplication.class })
 public class TestRHttpClient {
     private URI uri;
     private Properties properties;
 
     @Before
     public void init() throws URISyntaxException {
-//        uri = new URI("http://192.168.40.179:8998");
-        uri = new URI("http://192.168.10.63:31998");
-//        uri = new URI("http://192.168.10.65:8998");
+        String livyPort = "http://192.168.1.10:8080";
+        uri = new URI(livyPort);
+        // 创建properties 自动配置相关文件
         properties = new Properties();
         // 创建一个sparkr属性
-        properties.setProperty(RHttpConf.Entry.CONNECTION_SESSION_KIND.key(),"sparkr");
-        properties.setProperty(RHttpConf.Entry.CONNECTION_SESSION_MAX_SIZE.key(),"1");
+        properties.setProperty(RHttpConf.Entry.CONNECTION_SESSION_KIND.key(), "sparkr");
+        properties.setProperty(RHttpConf.Entry.CONNECTION_SESSION_MAX_SIZE.key(), "1");
     }
+
     @Test
     public void startTest() throws IOException, URISyntaxException, InterruptedException {
         RHttpClient rHttpClient = new RHttpClient(uri, new RHttpConf(properties));
         StringBuffer code = new StringBuffer();
-        code.append("library(featen)")
-                .append("\nlibrary(jsonlite)")
-                .append("\nan_start = \"2019/11/14\"")
-                .append("\nhistorystatrtime =  as.numeric(as.POSIXct(an_start, format=\"%Y/%m/%d\")) - 30 * 24 * 60* 60")
+        code.append("library(featen)").append("\nlibrary(jsonlite)").append("\nan_start = \"2019/11/14\"").append(
+                "\nhistorystatrtime =  as.numeric(as.POSIXct(an_start, format=\"%Y/%m/%d\")) - 30 * 24 * 60* 60")
                 .append("\nhistoryendtime =  as.numeric(as.POSIXct(an_start, format=\"%Y/%m/%d\")) + 7 * 24 * 60* 60")
                 .append("\nentityid = \"839055\"")
                 .append("\nData_history = featen.getHistoryData(entityid,as.character(historystatrtime),as.character(historyendtime),spark)")
-                .append("\nVData = Data_history$historydataDataFrame")
-                .append("\ncache(VData)")
+                .append("\nVData = Data_history$historydataDataFrame").append("\ncache(VData)")
                 .append("\nSensor = Data_history$sensorDataFrame")
                 .append("\ncreateOrReplaceTempView(Sensor, \"sensor\")")
                 .append("\nnames <- collect(sql(\"select collect_list(name) from sensor\"))[[1]][[1]]")
-                .append("\ncache(Sensor)")
-                .append("\nMData = featen.preprocess(VData,c(0:(length(names)-1)))")
-                .append("\ncache(MData)")
-                .append("\nnames(MData) <-  as.character(c(\"time\",names))")
-                .append("\nstime_ana = historyendtime - 7 * 24 * 60* 60")
-                .append("\netime_ana = historyendtime")
-                .append("\nsensorname = \"IC\"")
-                .append("\nresult_2_5 <- Feature_2_5(MData,Sensor,stime_ana,etime_ana)")
+                .append("\ncache(Sensor)").append("\nMData = featen.preprocess(VData,c(0:(length(names)-1)))")
+                .append("\ncache(MData)").append("\nnames(MData) <-  as.character(c(\"time\",names))")
+                .append("\nstime_ana = historyendtime - 7 * 24 * 60* 60").append("\netime_ana = historyendtime")
+                .append("\nsensorname = \"IC\"").append("\nresult_2_5 <- Feature_2_5(MData,Sensor,stime_ana,etime_ana)")
                 .append("\ncreateOrReplaceTempView(result_2_5, \"result_2_5\")")
                 .append("\nresult<-toJSON(collect(sql(\"select time,result from result_2_5\")))")
                 .append("\nreturn(result)");
-//                .append("\nreturn(collect(result_2_5))");
-//                .append("\nreturn(toJSON(collect(result)))");
+        // .append("\nreturn(collect(result_2_5))");
+        // .append("\nreturn(toJSON(collect(result)))");
         JobHandle submitcode = rHttpClient.submitcode(code.toString());
         submitcode.addListener(new JobHandle.Listener<StatementResultWithCode>() {
             @Override
@@ -85,18 +80,19 @@ public class TestRHttpClient {
             }
 
             @Override
-            public void onJobSucceeded(JobHandle<StatementResultWithCode> jobHandle, StatementResultWithCode statementResultWithCode) {
+            public void onJobSucceeded(JobHandle<StatementResultWithCode> jobHandle,
+                    StatementResultWithCode statementResultWithCode) {
                 System.out.println(statementResultWithCode.id);
                 System.out.println(statementResultWithCode.progress);
                 System.out.println(statementResultWithCode.state);
-//                try {
-//                    Output output = new Output(new FileOutputStream("data/person.bin"));
-//                    Kryo kryo = new Kryo();
-//                    kryo.register(StatementResultWithCode.class);
-//                    kryo.writeObject(output,statementResultWithCode);
-//                } catch (FileNotFoundException e) {
-//                    e.printStackTrace();
-//                }
+                // try {
+                // Output output = new Output(new FileOutputStream("data/person.bin"));
+                // Kryo kryo = new Kryo();
+                // kryo.register(StatementResultWithCode.class);
+                // kryo.writeObject(output,statementResultWithCode);
+                // } catch (FileNotFoundException e) {
+                // e.printStackTrace();
+                // }
                 if (statementResultWithCode.output.data == null) {
                     throw new RuntimeException("返回数据错误");
                 }
@@ -113,35 +109,27 @@ public class TestRHttpClient {
         TimeUnit.SECONDS.sleep(200000);
     }
 
-
     @Test
     public void startWithClientResultClassTest() throws IOException, URISyntaxException, InterruptedException {
         RHttpClient rHttpClient = new RHttpClient(uri, new RHttpConf(properties));
         StringBuffer code = new StringBuffer();
-        code.append("library(featen)")
-                .append("\nlibrary(jsonlite)")
-                .append("\nan_start = \"2019/11/14\"")
-                .append("\nhistorystatrtime =  as.numeric(as.POSIXct(an_start, format=\"%Y/%m/%d\")) - 30 * 24 * 60* 60")
+        code.append("library(featen)").append("\nlibrary(jsonlite)").append("\nan_start = \"2019/11/14\"").append(
+                "\nhistorystatrtime =  as.numeric(as.POSIXct(an_start, format=\"%Y/%m/%d\")) - 30 * 24 * 60* 60")
                 .append("\nhistoryendtime =  as.numeric(as.POSIXct(an_start, format=\"%Y/%m/%d\")) + 7 * 24 * 60* 60")
                 .append("\nentityid = \"839055\"")
                 .append("\nData_history = featen.getHistoryData(entityid,as.character(historystatrtime),as.character(historyendtime),spark)")
-                .append("\nVData = Data_history$historydataDataFrame")
-                .append("\ncache(VData)")
+                .append("\nVData = Data_history$historydataDataFrame").append("\ncache(VData)")
                 .append("\nSensor = Data_history$sensorDataFrame")
                 .append("\ncreateOrReplaceTempView(Sensor, \"sensor\")")
                 .append("\nnames <- collect(sql(\"select collect_list(name) from sensor\"))[[1]][[1]]")
-                .append("\ncache(Sensor)")
-                .append("\nMData = featen.preprocess(VData,c(0:(length(names)-1)))")
-                .append("\ncache(MData)")
-                .append("\nnames(MData) <-  as.character(c(\"time\",names))")
-                .append("\nstime_ana = historyendtime - 7 * 24 * 60* 60")
-                .append("\netime_ana = historyendtime")
-                .append("\nsensorname = \"IC\"")
-                .append("\nresult_2_5 <- Feature_2_5(MData,Sensor,stime_ana,etime_ana)")
+                .append("\ncache(Sensor)").append("\nMData = featen.preprocess(VData,c(0:(length(names)-1)))")
+                .append("\ncache(MData)").append("\nnames(MData) <-  as.character(c(\"time\",names))")
+                .append("\nstime_ana = historyendtime - 7 * 24 * 60* 60").append("\netime_ana = historyendtime")
+                .append("\nsensorname = \"IC\"").append("\nresult_2_5 <- Feature_2_5(MData,Sensor,stime_ana,etime_ana)")
                 .append("\ncreateOrReplaceTempView(result_2_5, \"result_2_5\")")
                 .append("\nresult<-toJSON(collect(sql(\"select time,result_2_5 from result_2_5\")))")
                 .append("\nreturn(result)");
-        JobHandle submitcode = rHttpClient.submitcode(code.toString(),ClientResultClass.class);
+        JobHandle submitcode = rHttpClient.submitcode(code.toString(), ClientResultClass.class);
         submitcode.addListener(new JobHandle.Listener<ClientResultClass>() {
             @Override
             public void onJobQueued(JobHandle<ClientResultClass> jobHandle) {
@@ -174,26 +162,28 @@ public class TestRHttpClient {
 
         TimeUnit.SECONDS.sleep(200000);
     }
-    public static class ResultClass{
+
+    public static class ResultClass {
         public long time;
         public int result;
-//        public ResultClass(){
-//            this(0,0);
-//        }
-//        public ResultClass(long time, int result) {
-//            this.time = time;
-//            this.result = result;
-//        }
+        // public ResultClass(){
+        // this(0,0);
+        // }
+        // public ResultClass(long time, int result) {
+        // this.time = time;
+        // this.result = result;
+        // }
     }
-    public static class ResultJsonClass{
+
+    public static class ResultJsonClass {
         @JsonProperty
         public Map<String, ResultClass[]> data;
-//        public ResultJsonClass(){
-//
-//        }
-////        public void setData(Map<String, List> data) {
-////            this.data = data;
-////        }
+        // public ResultJsonClass(){
+        //
+        // }
+        //// public void setData(Map<String, List> data) {
+        //// this.data = data;
+        //// }
 
         public int execution_count; // 0
         public String status;// 'OK'
@@ -201,11 +191,14 @@ public class TestRHttpClient {
         public String evalue;
         public List traceback;
     }
-    public static class ClientResultClass extends MyMessage.StatementResultWithCode{
+
+    public static class ClientResultClass extends MyMessage.StatementResultWithCode {
         public final ResultJsonClass output;
+
         public ClientResultClass() {
             this(null);
         }
+
         public ClientResultClass(ResultJsonClass output) {
             this.output = output;
         }
@@ -219,9 +212,7 @@ public class TestRHttpClient {
         code.append("library(featen)").append("\nfamilies <- c(\"gaussian\", \"poisson\")")
                 .append("\ntrain <- function(family) {")
                 .append("\n  model <- glm(Sepal.Length ~ Sepal.Width + Species, iris, family = family)")
-                .append("\n  summary(model)")
-                .append("\n}")
-                .append("\nmodel.summaries <- spark.lapply(families, train)")
+                .append("\n  summary(model)").append("\n}").append("\nmodel.summaries <- spark.lapply(families, train)")
                 .append("\nprint(model.summaries)");
         JobHandle submitcode = rHttpClient.submitcode(code.toString());
         submitcode.addListener(new JobHandle.Listener<StatementResultWithCode>() {
@@ -246,7 +237,8 @@ public class TestRHttpClient {
             }
 
             @Override
-            public void onJobSucceeded(JobHandle<StatementResultWithCode> jobHandle, StatementResultWithCode statementResultWithCode) {
+            public void onJobSucceeded(JobHandle<StatementResultWithCode> jobHandle,
+                    StatementResultWithCode statementResultWithCode) {
                 System.out.println(statementResultWithCode.id);
                 System.out.println(statementResultWithCode.progress);
                 System.out.println(statementResultWithCode.state);
